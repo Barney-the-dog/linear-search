df0ab93750ec98b6669f3d473cc99ac2b3774f34
Irina Ashurova <you-ve_got@mail.ru>
2023-04-09 20:17:26 -0400

    done

--- linear-search.js
+++ linear-search.js
@@ -2,13 +2,23 @@
 //принимает два аргумента: объект и массив. Она должна последовательно перебирать каждый элемент и возвращать индекс,
 // в котором объект был изначально найден. Если объект не найден, то функция возвращает значение -1.
 function linearSearch(n, arr) {
-
-}
-
+  for (let i = 0; i< arr.length; i++){
+    if (arr[i] == n ) {
+      return i
+    } 
+  }
+      return -1
+  }
 
 //напишем новый метод globalLinearSearch, который возвращает массив всех индексов, в которых найден элемент.
 function globalLinearSearch(n, arr) {
-
+  const indexes = []
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === n) {
+          indexes.push(i)
+      }
+  }
+  return indexes
 }
 
 module.exports = {
--- spec/linear-search.spec.js
+++ spec/linear-search.spec.js
@@ -6,6 +6,11 @@ describe("linearSearch()", function() {
     let arr = [1,2,3,4,5];
     expect(linearSearch(item, arr)).toEqual(2);
   });
+  it("returns -1 if not in array", function() {
+    let item = 6;
+    let arr = [1,2,3,4,5];
+    expect(linearSearch(item, arr)).toEqual(-1);
+  });
 });
 
 describe("globalLinearSearch()", function() {
@@ -13,5 +18,10 @@ describe("globalLinearSearch()", function() {
     let item = 'a';
     let arr = ['b', 'a', 'n', 'a', 'n', 'a', 's'];
     expect(globalLinearSearch(item, arr)).toEqual([1, 3, 5]);
+  });
+  it("returns empty array if not in array", function() {
+    let item = 'c';
+    let arr = ['b', 'a', 'n', 'a', 'n', 'a', 's'];
+    expect(globalLinearSearch(item, arr)).toEqual([]);
   });
 });
